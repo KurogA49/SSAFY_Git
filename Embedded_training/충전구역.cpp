0d0dbc6d0f41bb22dcb1e37b684e5f564500da91
KurogA49 <didrjsgh12@gmail.com>
2023-02-22 17:54:17 +0900

    230222 추가

--- Embedded_training/충전구역.cpp
+++ Embedded_training/충전구역.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 struct node {
-    int y;
-    int x;
+	int y;
+	int x;
 	int range;
 	int power;
 };
@@ -15,8 +15,8 @@ struct p {
 	int x;
 };
 
-int dy[5] = { 0, -1, 1, 0, 0 };
-int dx[5] = { 0, 0, 0, -1, 1 };
+int dy[5] = { 0, -1, 0, 1, 0 };
+int dx[5] = { 0, 0, 1, 0, -1 };
 
 int M, A, chargeA, chargeB, Max;
 vector<int> trackA, trackB;
@@ -41,91 +41,109 @@ int getDistance(int y1, int x1, int y2, int x2)
 	return dify + difx;
 }
 
-void dfs(int lev, p userA, p userB)
+int getTotalPower()
 {
-	if (lev == M)
+	int power = 0;
+	for (int currentTime = 0; currentTime <= M; currentTime++)
 	{
-		if (Max < chargeA + chargeB)
-			Max = chargeA + chargeB;
-		return;
-	}
+		// 현재 두 사람이 충전 구역 내에 있는가?
+		int maxA = 0;
+		int maxB = 0;
+		int tmpA = 0;
+		int tmpB = 0;
+		for (int i = 0; i < BC.size(); i++)
+		{
+			int fromA = getDistance(userA.y, userA.x, BC[i].y, BC[i].x);
+			int fromB = getDistance(userB.y, userB.x, BC[i].y, BC[i].x);
+			
+			// 혹시 같은 충전 구역 내에 두 사람이 모두 있다면
+			// 생각할 수 있는 경우
+			// A의 현재 위치가 겹쳐져 있으면 A가 버리고 B에게 몰아주는 경우
+			// B의 현재 위치가 겹쳐져 있으면 B가 버리고 A에게 몰아주는 경우
+			if (fromA <= BC[i].range && fromB <= BC[i].range)
+			{
+				if (tmpA < BC[i].power / 2)
+					tmpA = BC[i].power / 2;
+				if (tmpB < BC[i].power / 2)
+					tmpB = BC[i].power / 2;
+			}
+			// 서로 떨어져 있다면
+			else
+			{
+				if (fromA <= BC[i].range && maxA < BC[i].power)
+				{
+					maxA = BC[i].power;
+				}
+					
+				if (fromB <= BC[i].range && maxB < BC[i].power)
+				{
+					maxB = BC[i].power;
+				}
+					
+			}
+		}
+		if (maxA > tmpA)
+		{
+			if (maxB < tmpB * 2)
+				maxB = tmpB * 2;
+		}
+		else if (maxB > tmpB)
+		{
+			if (maxA < tmpA * 2)
+				maxA = tmpA * 2;
+		}
+		else 
+		{
+			if (maxA < tmpA * 2)
+				maxA = tmpA * 2;
+			else if (maxB < tmpB * 2)
+				maxB = tmpB * 2;
+		}
+		power += maxA;
+		power += maxB;
 
-	// A와 B는 현재 구역에서 충전 가능한지 확인한 뒤 충전
-	for (int i = 0; i < BC.size(); i++)
-	{
-		// 혹시 A와 B가 같은 충전 구역에 있는 경우도 고려하여야 함
-		if (getDistance(userA.y, userA.x, BC[i].y, BC[i].x) <= BC[i].range && getDistance(userB.y, userB.x, BC[i].y, BC[i].x) <= BC[i].range)
-		{
-			chargeA += BC[i].power / 2;
-			chargeB += BC[i].power / 2;
-			userA.y += dy[trackA[lev]];
-			userA.x += dx[trackA[lev]];
-			userB.y += dy[trackB[lev]];
-			userB.x += dx[trackB[lev]];
-			dfs(lev + 1, userA, userB);
-			chargeA -= BC[i].power / 2;
-			chargeB -= BC[i].power / 2;
-		}
-		else
-		{
-			if (getDistance(userA.y, userA.x, BC[i].y, BC[i].x) <= BC[i].range)
-			{
-				chargeA += BC[i].power / 2;
-				userA.y += dy[trackA[lev]];
-				userA.x += dx[trackA[lev]];
-				userB.y += dy[trackB[lev]];
-				userB.x += dx[trackB[lev]];
-				dfs(lev + 1, userA, userB);
-				chargeA -= BC[i].power / 2;
-			}
-			if (getDistance(userB.y, userB.x, BC[i].y, BC[i].x) <= BC[i].range)
-			{
-				chargeB += BC[i].power / 2;
-				userA.y += dy[trackA[lev]];
-				userA.x += dx[trackA[lev]];
-				userB.y += dy[trackB[lev]];
-				userB.x += dx[trackB[lev]];
-				dfs(lev + 1, userA, userB);
-				chargeB -= BC[i].power / 2;
-			}
-		}
+		if (currentTime == M)
+			break;
+
+		// 다음으로 이동
+		userA.y += dy[trackA[currentTime]];
+		userA.x += dx[trackA[currentTime]];
+		userB.y += dy[trackB[currentTime]];
+		userB.x += dx[trackB[currentTime]];
 	}
+	return power;
 }
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
+
 	cin >> T;
-	
+
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		cin >> M >> A;
-
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < M; i++)
 		{
-			for (int j = 0; j < M; j++)
-			{
-				int t;
-				cin >> t;
-				if (i == 0)
-					trackA.push_back(t);
-				else if (t == 1)
-					trackB.push_back(t);
-			}
+			int t; cin >> t;
+			trackA.push_back(t);
+		}
+		for (int i = 0; i < M; i++)
+		{
+			int t; cin >> t;
+			trackB.push_back(t);
 		}
 		for (int i = 0; i < A; i++)
 		{
 			node t;
-			cin >> t.y >> t.x >> t.range >> t.power;
+			cin >> t.x >> t.y >> t.range >> t.power;
 			BC.push_back(t);
 		}
-		userA = p{ 0, 0 };
-		userB = p{ 10, 10 };
-		Max = 0;
-		dfs(0, userA, userB);
-		cout << '#' << test_case << " " << Max << '\n';
+		userA = { 1, 1 };
+		userB = { 10, 10 };
+
+		cout << '#' << test_case << " " << getTotalPower() << '\n';
 		trackA.clear();
 		trackB.clear();
 		BC.clear();
