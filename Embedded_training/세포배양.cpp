77790790ee6befdbcb7258eebba2a897d68b3708
MayhemY <didrjsgh12@gmail.com>
2023-03-02 23:43:42 +0900

    230302 저녁 정리

--- Embedded_training/세포배양.cpp
+++ Embedded_training/세포배양.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <queue>
 #include<algorithm>
@@ -17,39 +18,49 @@ struct cell {
 	int life;
 	int timestamp;
 	bool isAlive;
+	bool operator < (cell right) const
+	{
+		return life < right.life;
+	}
 };
 
 int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
 
-cell arr[351][351];
-int board[351][351];
-int N, M, K, X;
+cell arr[651][651];
+int N, M, K, X, cnt;
+int sy, sx, ey, ex;
 
-void cultivate()
+void cultivate(int time)
 {
-	queue<cell> q;
-
-	for (int i = 0; i < 351; i++)
+	priority_queue<cell> q;
+	if (time == 0)
 	{
-		for (int j = 0; j < 351; j++)
+		ey = 650; ex = 650;
+		sy = 0; sx = 0;
+	}
+	for (int i = sy; i <= ey; i++)
+	{
+		for (int j = sx; j <= ex; j++)
 		{
-			if(arr[i][j].isAlive && arr[i][j].life > 0)
+			if (arr[i][j].isAlive && arr[i][j].life > 0)
 				q.push(arr[i][j]);
 		}
+	}
+	if (time == 0)
+	{
+		sy = 650; sx = 650;
+		ey = 0; ex = 0;
 	}
 	int len = q.size();
 	for (int i = 0; i < len; i++)
 	{
-		cell now = q.front(); q.pop();
+		cell now = q.top(); q.pop();
+
 		if (now.timestamp == now.life * 2)
 			arr[now.y][now.x].isAlive = false;
 		arr[now.y][now.x].timestamp++;
-		q.push(now);
-	}
-	for (int i = 0; i < len; i++)
-	{
-		cell now = q.front(); q.pop();
+
 		if (now.timestamp == now.life + 1)
 		{
 			for (int i = 0; i < 4; i++)
@@ -57,56 +68,65 @@ void cultivate()
 				int cy = now.y + dy[i];
 				int cx = now.x + dx[i];
 
-				if (cy < 0 || cx < 0 || cy >= 351 || cx >= 351) continue;
-				if (arr[cy][cx].life != 0)
-				{
-					if (arr[cy][cx].life < now.life && arr[cy][cx].timestamp == 1)
-						arr[cy][cx] = { cy, cx, now.life, 1, true };
-				}
-				else
-					arr[cy][cx] = { cy, cx, now.life, 1, true };
+				if (cy < 0 || cx < 0 || cy >= 650 || cx >= 650) continue;
+				if (arr[cy][cx].life != 0) continue;
+				arr[cy][cx] = { cy, cx, now.life, 1, true };
+				if (sy > cy)
+					sy = cy;
+				if (sx > cx)
+					sx = cx;
+				if (ey < cy)
+					ey = cy;
+				if (ex < cx)
+					ex = cx;
 			}
+		}
+		else
+		{
+			if (now.y < sy)
+				sy = now.y;
+			if (now.x < sx)
+				sx = now.x;
+			if (now.y > ey)
+				ey = now.y;
+			if (now.x > ex)
+				ex = now.x;
 		}
 	}
 }
 
 int main()
 {
-	// freopen("C:\\Users\\SSAFY\\Downloads\\sample_input (8).txt", "r", stdin);
+	// freopen("C:\\Users\\User\\Downloads\\sample_input (6).txt", "r", stdin);
 	int T;
 	cin >> T;
 	for (int test_case = 1; test_case <= T; test_case++)
 	{
 		cin >> N >> M >> K;
-		for (int i = 150; i < 150 + N; i++)
+		for (int i = 300; i < 300 + N; i++)
 		{
-			for (int j = 150; j < 150 + M; j++)
+			for (int j = 300; j < 300 + M; j++)
 			{
 				int t; cin >> t;
-				arr[i][j] = { i, j, t, 0, true };
+				arr[i][j] = { i, j, t, 1, true };
 			}
 		}
-		for (int i = 0; i <= K; i++)
+		cnt = 0;
+		
+		for (int i = 0; i < K; i++)
 		{
-			cultivate();
+			cultivate(i);
 		}
-		int cnt = 0;
-		for (int i = 0; i < 351; i++)
+		for (int i = 0; i < 650; i++)
 		{
-			for (int j = 0; j < 351; j++)
+			for (int j = 0; j < 650; j++)
 			{
-				if (arr[i][j].isAlive)
+				if (arr[i][j].isAlive && arr[i][j].life > 0)
 					cnt++;
 			}
 		}
 		cout << "#" << test_case << " " << cnt << "\n";
-		for (int i = 0; i < 351; i++)
-		{
-			for (int j = 0; j < 351; j++)
-			{
-				arr[i][j] = { 0, 0, 0, 0, true };
-			}
-		}
+		memset(arr, 0, sizeof(arr));
 	}
 
 	return 0;
--- Embedded_training/점심시간.cpp
+++ Embedded_training/점심시간.cpp
@@ -8,8 +8,7 @@
 using namespace std;
 
 struct people {
-	int y;
-	int x;
+	int y, x;
 	int distToSt1, distToSt2;
 };
 
@@ -21,130 +20,101 @@ struct stair {
 int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
 
-
-vector<people> v;
-vector<stair> s;
-vector<int> p1, p2;
-int arr[10][10];
-int dat[11];
-int N, MinT = 21e8;
+int N, MinT = 21e8;;
+vector<people> members;
+vector<stair> stairs;
+vector<int> st1, st2;
 
 int getDistance(int y1, int x1, int y2, int x2)
 {
 	int dify = y1 - y2 > 0 ? y1 - y2 : y2 - y1;
 	int difx = x1 - x2 > 0 ? x1 - x2 : x2 - x1;
-
 	return dify + difx;
 }
 
-// 모든 인원들에 대해 두 계단으로 향하는 시간을 각각 기록
-void solve()
+void getTimeToStair()
 {
-	for (int i = 0; i < 2; i++)
+	int size = members.size();
+
+	for (int i = 0; i < size; i++)
 	{
-		stair snow = s[i];
-		int size = v.size();
-		for (int j = 0; j < size; j++)
-		{
-			people pnow = v[j];
-			if (i == 0)
-			{
-				v[j].distToSt1 = getDistance(pnow.y, pnow.x, snow.y, snow.x);
-			}
-			else if (i == 1)
-			{
-				v[j].distToSt2 = getDistance(pnow.y, pnow.x, snow.y, snow.x);
-			}
-		}
+		members[i].distToSt1 = getDistance(members[i].y, members[i].x, stairs[0].y, stairs[0].x);
+		members[i].distToSt2 = getDistance(members[i].y, members[i].x, stairs[1].y, stairs[1].x);
 	}
 }
 
-
-int getMinTime(vector<int> st1, vector<int> st2)
+int getTotalTime(vector<int> st1, vector<int> st2)
 {
-	// 두 계단 벡터를 입력받기
-	// 계단 벡터의 내용은 각각의 인원이 그 계단으로 향하는 시간
-	int sz1 = st1.size();
-	int sz2 = st2.size();
-	int tmp1 = 0, tmp2 = 0;
-	// 시간이 작은 순서대로 정렬
 	sort(st1.begin(), st1.end());
 	sort(st2.begin(), st2.end());
+	int tmp1 = 0; int tmp2 = 0;
+	int sz1 = st1.size(); int sz2 = st2.size();
 
-	// 두 계단에 대해,
 	for (int i = 0; i < sz1; i++)
 	{
-		// 현재 몰려 있는 인원이 3명 이상이라면
 		if (i >= 3)
 		{
-			// 그리고 i번째 사람이 계단으로 향하는 시간보다 
-			if (st1[i - 3] + s[0].time > st1[i]) {
-				st1[i] = st1[i - 3] + s[0].time;
-			}
+			if (st1[i] < st1[i - 3] + stairs[0].time)
+				st1[i] = st1[i - 3] + stairs[0].time;
 		}
 	}
-
 	for (int i = 0; i < sz2; i++)
 	{
 		if (i >= 3)
 		{
-			if (st2[i - 3] + s[0].time > st2[i]) {
-				st2[i] = st2[i - 3] + s[0].time;
-			}
+			if (st2[i] < st2[i - 3] + stairs[1].time)
+				st2[i] = st2[i - 3] + stairs[1].time;
 		}
 	}
 
-	if (sz1 != 0)
-		tmp1 = st1[sz1 - 1] + s[0].time;
+	// 두 계단의 총 이동 시간은 각각
+	// (마지막 사람이 계단까지 이동 및 대기한 시간) + (계단 자체의 이동 시간)
+	if (sz1 > 0)
+		tmp1 = st1[sz1 - 1] + stairs[0].time;
 	else
 		tmp1 = 0;
 
-	if (sz2 != 0)
-		tmp2 = st2[sz2 - 1] + s[1].time;
+	if (sz2 > 0)
+		tmp2 = st2[sz2 - 1] + stairs[1].time;
 	else
 		tmp2 = 0;
-
 	return max(tmp1, tmp2);
 }
 
-// dfs 탐색
 void dfs(int now)
 {
-	// 모든 인원을 두 계단에 배정했다면
-	if (now == v.size())
+	if (now == members.size())
 	{
-		// 이 경우에 대한 최소 소요 시간을 계산
-		int result = getMinTime(p1, p2);
-		// 만약 현재 최소 시간보다 더 적다면 갱신
-		MinT = min(MinT, result);
+		int t = getTotalTime(st1, st2);
+		MinT = min(t, MinT);
 		return;
 	}
-	people pnow = v[now];
-	// 현재 pnow 사람이 계단 1 혹은 2로 향하는 두 경우를 선정
-	// 각각의 계단 벡터에 이동 소요 시간을 등록 및 다음 dfs
+
 	for (int i = 0; i < 2; i++)
 	{
-		if (i == 1)
+		if (i == 0)
 		{
-			p2.push_back(pnow.distToSt2);
-			dat[now]++;
+			st1.push_back(members[now].distToSt1);
 			dfs(now + 1);
-			dat[now]--;
-			p2.pop_back();
-			
+			st1.pop_back();
 		}
-		else if (i == 0)
+		else if (i == 1)
 		{
-			p1.push_back(pnow.distToSt1);
+			st2.push_back(members[now].distToSt2);
 			dfs(now + 1);
-			p1.pop_back();
+			st2.pop_back();
 		}
 	}
 }
 
+// 문제 풀이 방향
+// 모든 사람들에 대해 각각의 계단으로 향하는 이동 시간을 기록
+// dfs를 이용하여 어떤 사람이 어떤 계단으로 이동하는지 모든 경우를 상정
+// 한 계단에 3명 이상의 인원이 몰릴 경우 delay 되는 문제를 반영
+
 int main()
 {
-	// freopen("C:\\Users\\SSAFY\\Downloads\\sample_input (8).txt", "r", stdin);
+	// ("C:\\Users\\User\\Downloads\\sample_input (8).txt", "r", stdin);
 	int T;
 	cin >> T;
 	for (int test_case = 1; test_case <= T; test_case++)
@@ -154,18 +124,21 @@ int main()
 		{
 			for (int j = 0; j < N; j++)
 			{
-				cin >> arr[i][j];
-				// 사람이면 사람 벡터에
-				// 계단이면 계단 벡터에 등록
-				if (arr[i][j] == 1)
-					v.push_back({ i, j, 0, 0 });
-				else if (arr[i][j] != 0)
-					s.push_back({ i, j, arr[i][j] });
+				int t; cin >> t;
+				if (t == 1)
+					members.push_back({ i, j, 0, 0 });
+				else if (t > 1)
+					stairs.push_back({ i, j, t });
 			}
 		}
-		solve();
+		getTimeToStair();
 		dfs(0);
+		
 		cout << "#" << test_case << " " << MinT + 1 << "\n";
+		members.clear();
+		stairs.clear();
+		st1.clear(); st2.clear();
+		MinT = 21e8;
 	}
 
 	return 0;
